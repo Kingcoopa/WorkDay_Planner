39cff1f71a24c4e4b4629bbe68d51ded5e517334
Kingcoopa <69878838+Kingcoopa@users.noreply.github.com>
2021-03-04 16:06:47 -0500

    Initial JS Logic

---
+++ Assets/script.js
@@ -0,0 +1,29 @@
+// Utilize moment to get to the date and time at the current time. This data will be placed in 2 variables.
+var date = moment().format('dddd, MMMM Do, YYYY');
+var curHour= parseInt(moment().format('H'));
+var enterTask = $(".enterTask");
+
+  // Current day beneath header.
+  $("#currentDay").text(date);
+
+  // Color changing function for the rows.
+  enterTask.each(function () {
+
+// Cross-checks the hour block time against current time.
+var hour = $(this).parent('.row');
+var hr = parseInt(hour.attr('id'));
+    
+    //  Cross checks if current hour is greater than the time of the block.
+    if (hr < curHour) {
+        $(this).addClass('expired');
+    
+    // Cross checks if current hour matches the time of the block.
+    } else if (hr === curHour) {
+        $(this).removeClass('expired');
+        $(this).addClass('current');
+    }   else {
+        $(this).removeClass('expired');
+        $(this).removeClass('current');
+        $(this).addClass('future');
+    }
+});
